b491367a7d197a3c2fd6e3c5dcb4641d56b56606
Fernando Sánchez <guinea90@gmail.com>
2022-04-26 07:00:38 -0500

    actualizacion Historia y Menu

--- Desktop/UCamp/restaurante/src/components/Historia.jsx
+++ Desktop/UCamp/restaurante/src/components/Historia.jsx
@@ -5,39 +5,14 @@ import React from "react";
 // } from "react-router-dom";
 // import ListaCursos from "./Cursos"
 
-const cursos = [
-    {
-        id: "1",
-        name: "Básico"
-    },
-    {
-        id: "2",
-        name: "Intermedio"
-    },
-    {
-        id: "3",
-        name: "Avanzado"
-    },
-    {
-        id: "4",
-        name: "Desarrollo Front-end"
-    },
-    {
-        id: "5",
-        name: "Desarrollo Back-end"
-    },
-]
-// console.log(cursos)
-
-
 export default function Historia () {
-    return(
-        <div className="cursos">
-            <ul className="cursos1">
-                {cursos.map((curso) => (
-                <a href='./home'><li className="caja2" key={curso.id}><h3>{curso.name}</h3></li></a>
-                ))}
-            </ul>
+    return (
+        <div className="historia">
+            <div><h2>Nuestra</h2></div>
+            <div><h1>Historia</h1></div>
+            <div><p>Fundado en 1959, cuenta 3 restaurantes en México, donde lo esperamos para que comparta con nosotros la pasión por la cocina internacional. En Sucursales encontrará las direcciones, teléfonos y horarios de servicio de cada uno de nuestros restaurantes. Nuestra meta es permitir que nuestros clientes compartan el placer de la preparación de nuestros extraordinarios platillos, haciendo que su visita se convierta en una experiencia inolvidable</p></div>
+            <div><p>Te hacemos sentir como en casa, con un servicio inigualable que nos ha caracterizado por más de 60 años. La amabilidad y profesionalismo de nuestro equipo de servicio, harán que tu estancia en cualquier a de nuestros restaurantes sea la mejor experiencia.</p></div>
+            <div><p>Al frente de las cocinas de Picabira hay un equipo de chef's de un sazón inigualable. Desde muy temprano, comienzan la deliciosa preparación de lo que se servirá en el día.</p></div>
         </div>
     )
 }
--- Desktop/UCamp/restaurante/src/components/Menu.jsx
+++ Desktop/UCamp/restaurante/src/components/Menu.jsx
@@ -1,29 +1,38 @@
 import React from "react";
+import pdfLaunch from 'src/components/LaunchMenu.pdf'
 
-const menus = [
-    {
-        id: "1",
-        name: "Desayuno"
-    },
-    {
-        id: "2",
-        name: "Comida y Cena"
-    },
-    {
-        id: "3",
-        name: "Bebidas"
-    },
-]
+// const menus = [
+//     {
+//         id: "1",
+//         name: "Desayuno"
+//     },
+//     {
+//         id: "2",
+//         name: "Comida y Cena"
+//     },
+//     {
+//         id: "3",
+//         name: "Bebidas"
+//     },
+// ]
 
 
 export default function Menu () {
     return(
-        <div className="cursos">
+        // <div className="cursos">
+        //     <ul className="cursos1">
+        //         {menus.map((menus) => (
+        //         <a href='./home'><li className="caja2" key={menus.id}><h3>{menus.name}</h3></li></a>
+        //         ))}
+        //     </ul>
+        // </div>
+
+            <div className='cursos'>
             <ul className="cursos1">
-                {menus.map((menus) => (
-                <a href='./home'><li className="caja2" key={menus.id}><h3>{menus.name}</h3></li></a>
-                ))}
+                <a href={pdfLaunch} target="blank" download="LaunchMenu.pdf"><li className="caja2"><h3>Desayuno</h3></li></a>
+                <a href="https://www.restauranteelbajio.com.mx/uploads/desayunos.pdf?uid=572bf0df8fb611881448cbc222ea149e"><li className="caja2"><h3>Comida y Cena</h3></li></a>
+                <a href="https://www.restauranteelbajio.com.mx/uploads/aguas.pdf?uid=572bf0df8fb611881448cbc222ea149e"><li className="caja2"><h3>Bebidas</h3></li></a>
             </ul>
-        </div>
+            </div>
     )
 }
